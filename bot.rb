9613c7f421419a791d119b8e2302d560c95d22ca
Eric Lujan <eric@lujan.io>
2014-11-06 23:05:46 -0800

    Added some basic hello world functionality.

--- bot.rb
+++ bot.rb
@@ -1,11 +1,24 @@
 require 'cinch'
 
-# Set some basic configuration and define the bot object,
+# Plugin definition
+class Hello
+    # Pull in dem dependencies
+    include Cinch::Plugin
+    # Look for the word "hello" in messages
+    match "hello"
+    # Actually do something with that
+    def execute(m)
+        m.reply "Hello, #{m.user.nick}"
+    end
+end
+
+# Set some basic configuration and define the bot object.
 bot = Cinch::Bot.new do
     configure do |c|
         c.server = "irc.freenode.net"
         c.channels = ["#lujan"]
         c.nick = "lujan-bot"
+        c.plugins.plugins = [Hello]
     end
 end
 
