077021194194fe3b065d3a35ef8870a4f98b609f
Eric Lujan <eric@lujan.io>
2014-11-08 00:43:08 -0800

    added a shebang

---
+++ ircbotd.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+require 'daemons'
+
+# Current directory
+pwd = File.dirname( File.expand_path(__FILE__))
+file = pwd + "/bot.rb"
+
+# Set some options
+options = {
+	:app_name    =>"ircbotd",
+	:multiple   => true,
+	:mode       => :load,
+	:log_output => true,
+	:monitor    => true
+}
+
+# Run the bot and daemonize it
+Daemons.run_proc('ircbotd',options) do
+	exec "ruby #{file}"
+end
